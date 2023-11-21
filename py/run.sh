35e0463c5fe7ae8994e804e03fad4527e01c9ef9
larry-init <lanreadeniji77@gmail.com>
2023-11-21 15:46:37 +0000

    Dockerfile

--- py/run.sh
+++ py/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ./py/scrap.py --buildurl ${BUILD_URL} && > ./py/log.html 2>&1 || echo "Big error"
+python3 ./py/scrap.py --buildurl ${BUILD_URL} > ./py/log.html 2>&1 || echo "Big error"
