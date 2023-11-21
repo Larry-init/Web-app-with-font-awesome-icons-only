5aba200e79d9e37a79337e5ac89d3b11d47adf3f
larry-init <lanreadeniji77@gmail.com>
2023-11-21 18:13:30 +0000

    d

--- py/scrap.py
+++ py/scrap.py
@@ -1,24 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 import argparse
-import sys
 
-parser = argparse.ArgumentParser(description='This helps to pass variables using the CLI')
-parser.add_argument('--buildurl', action='store', type=str, help='Enters the build URL')
+parser = argparse.ArgumentParser(description='This helps to pass variables using the cli')
+parser.add_argument('--buildurl', action='store', type=str, help='Enters the build url')
 args = parser.parse_args()
-buildurl = args.buildurl
+buildurl=args.buildurl
 
-url = f"{buildurl}/console"
+
+url=f"{buildurl}/console"
 
 username = 'pocket'
 password = 'pocket'
 
-try:
-    response = requests.get(url, auth=(username, password))
-    response.raise_for_status()  # Check for HTTP errors
-    soup = BeautifulSoup(response.text, 'lxml')
-    console = soup.find('pre', class_='console-output').text
-    print(console)
-except Exception as e:
-    print(f"Error: {e}", file=sys.stderr)
-    sys.exit(1)
+response=requests.get(url, auth=(username, password)).text
+soup=BeautifulSoup(response,'lxml')
+console=soup.find('pre',class_='console-output').text
+print(console)
