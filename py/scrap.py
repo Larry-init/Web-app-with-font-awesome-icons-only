5cffeb4e956c2f9f966a98a24dbee32fe76f8369
larry-init <lanreadeniji77@gmail.com>
2023-11-21 16:12:29 +0000

    Dockerfile

--- py/scrap.py
+++ py/scrap.py
@@ -3,19 +3,29 @@ import requests
 import argparse
 
 parser = argparse.ArgumentParser(description='This helps to pass variables using the cli')
-parser.add_argument('--buildurl', action='store', type=str, help='Enters the build url')
+parser.add_argument('--buildurl', action='store', type=str, help='Enters the build URL')
 args = parser.parse_args()
-buildurl=args.buildurl
+buildurl = args.buildurl
 
 print(buildurl)
 
-url=f"{buildurl}/console"
+url = f"{buildurl}/console"
 print(url)
+
 username = 'pocket'
 password = 'pocket'
 
-response=requests.get(url, auth=(username, password))
-url=response.text
-soup=BeautifulSoup(url,'lxml')
-console=soup.find('pre',class_='console-output').text
-print(console)
+try:
+    response = requests.get(url, auth=(username, password))
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    console_element = soup.find('pre', class_='console-output')
+
+    if console_element:
+        console = console_element.text
+        print(console)
+    else:
+        print("Console output not found.")
+except requests.RequestException as e:
+    print(f"Error: {e}")
