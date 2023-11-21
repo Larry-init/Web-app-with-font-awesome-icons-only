bd46acc5d9a8c243858e36a9af469e11c089929c
larry-init <lanreadeniji77@gmail.com>
2023-11-21 05:58:04 +0000

    dockerfile

--- py/scrap.py
+++ py/scrap.py
@@ -3,7 +3,7 @@ import requests
 import argparse
 
 parser = argparse.ArgumentParser(description='This helps to pass variables using the cli')
-parser.add_argument('buildurl', action='store', type=str, help='Enters the build url')
+parser.add_argument('--buildurl', action='store', type=str, help='Enters the build url')
 args = parser.parse_args()
 buildurl=args.buildurl
 
