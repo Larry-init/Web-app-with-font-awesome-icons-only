b1f1e6a1766c4fa080dcd501d247e74cab93e3bb
larry-init <lanreadeniji77@gmail.com>
2023-11-21 17:58:38 +0000

    d

--- py/run.sh
+++ py/run.sh
@@ -2,4 +2,5 @@
 
 echo ${BUILD_URL}
 echo $BUILD_URL
-python3 py/scrap.py --buildurl $BUILD_URL > py/log.html 2>&1 || echo "Big error"
+# python3 py/scrap.py --buildurl $BUILD_URL > py/log.html 2>&1 || echo "Big error"
+python3 /var/jenkins_home/workspace/myyg_master/py/scrap.py --buildurl $BUILD_URL > /var/jenkins_home/workspace/myyg_master/py/log.html 2>&1 || echo "Big error"
