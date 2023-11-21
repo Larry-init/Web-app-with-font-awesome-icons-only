e306a4a5357d7e8307aff34e105f436a5541909d
larry-init <lanreadeniji77@gmail.com>
2023-11-21 16:44:49 +0000

    d

--- py/scrap.py
+++ py/scrap.py
@@ -13,20 +13,8 @@ url=f"{buildurl}/console"
 username = 'pocket'
 password = 'pocket'
 
-# response=requests.get(url, auth=(username, password))
-# url=response.text
-# soup=BeautifulSoup(url,'lxml')
-# console=soup.find('pre',class_='console-output').text
-# print(console)
-
-try:
-    response = requests.get(url, auth=(username, password))
-    response.raise_for_status()
-
-    # Save the raw response content to a file
-    with open('log.html', 'wb') as log_file:
-        log_file.write(response.content)
-
-    print("Log saved to log.html")
-except requests.RequestException as e:
-    print(f"Error: {e}")
+response=requests.get(url, auth=(username, password))
+url=response.text
+soup=BeautifulSoup(url,'lxml')
+console=soup.find('pre',class_='console-output').text
+print(console)
