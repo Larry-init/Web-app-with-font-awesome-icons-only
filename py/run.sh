3d740f5ffc388c6e135642f3fd4b828e99c1e495
larry-init <lanreadeniji77@gmail.com>
2023-11-22 07:19:28 +0000

    d

--- py/run.sh
+++ py/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo ${BUILD_URL}
 echo $BUILD_URL
-python3 py/scrap.py --buildurl $BUILD_URL && echo $BUILD_URL > py/log.html 2>&1 || echo "Big error"
+python3 py/scrap.py --buildurl $BUILD_URL > py/log.html 2>&1 || echo "Big error"
